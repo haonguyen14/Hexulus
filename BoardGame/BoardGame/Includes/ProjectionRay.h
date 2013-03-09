2f08979a643457e9d175c159e9e66e77c67e3753
Hao Nguyen <hao_nguyen1412@yahoo.com.vn>
2013-03-09 15:43:53 -0800

    Another logic mistake with fovy variable. Also add constructor for projection ray supporting GLFrame camera

--- BoardGame/BoardGame/Includes/ProjectionRay.h
+++ BoardGame/BoardGame/Includes/ProjectionRay.h
@@ -1,6 +1,8 @@
 #include "Line3D.h"
 #include "Plane3D.h"
 
+#include "GLFrame.h"
+
 #ifdef __APPLE__
 #include <glut/glut.h>
 #else
@@ -13,7 +15,12 @@ class ProjectionRay : public Line3D
 public:
 	ProjectionRay();
 	ProjectionRay(Vector3D point, Vector3D direction);
+
+	//constructors are for picking ray special use
 	ProjectionRay(GLfloat mouseX, GLfloat mouseY, Vector3D cameraPos, Vector3D cameraLookAt, Vector3D cameraUp, GLfloat fovy, GLfloat aspect, GLfloat near, GLfloat far);
+	ProjectionRay(GLfloat mouseX, GLfloat mouseY, GLFrame cameraFrame, GLfloat fovy, GLfloat aspect, GLfloat near, GLfloat far);
+	//////////////////////////////////////////////////
+
 	~ProjectionRay();
 
 	Vector3D *planeIntersect(Plane3D plane);
--- BoardGame/BoardGame/ProjectionRay.cpp
+++ BoardGame/BoardGame/ProjectionRay.cpp
@@ -24,7 +24,7 @@ ProjectionRay::ProjectionRay(GLfloat _mouseX, GLfloat _mouseY, Vector3D cameraPo
 	//Let v and h are the axis on the Viewport (Near Plane Coordinate)
 	Vector3D cameraDir = cameraLookAt.sub(cameraPos).normalize();
 	
-	GLfloat vLength = (GLfloat)tanf((float)fovy) * nearPlane;
+	GLfloat vLength = (GLfloat)tanf((float)fovy / 2.0f) * nearPlane;
 	GLfloat hLength = vLength * (aspect);
 
 	Vector3D h = cameraDir.crossProduct(cameraUp).normalize().scalarMul(hLength); 
@@ -35,6 +35,40 @@ ProjectionRay::ProjectionRay(GLfloat _mouseX, GLfloat _mouseY, Vector3D cameraPo
 	mDirection = mPoint.sub(cameraPos);
 }
 
+ProjectionRay::ProjectionRay(GLfloat _mouseX, GLfloat _mouseY, GLFrame cameraFrame, GLfloat fovy, GLfloat aspect, GLfloat nearPlane, GLfloat farPlane)
+{
+	//Get viewport information
+	GLint viewport[4];
+	glGetIntegerv(GL_VIEWPORT, viewport);
+
+	//Convert Windows Coordinate to Viewport Coordinate (Near Plane Coordinate)
+	GLfloat mouseX = _mouseX - ((GLfloat)viewport[2] / 2.0f);
+	GLfloat mouseY = ((GLfloat)viewport[3] - _mouseY) - ((GLfloat)viewport[3] / 2.0f);
+
+	mouseX  = mouseX / ((GLfloat)viewport[2] / 2);
+	mouseY	= mouseY / ((GLfloat)viewport[3] / 2);
+
+	//Let v and h are the axis on the Viewport (Near Plane Coordinate)
+	Vector3D cameraPos((GLfloat)cameraFrame.GetOriginX(), (GLfloat)cameraFrame.GetOriginY(), (GLfloat)cameraFrame.GetOriginZ());
+	
+	float temp[3];
+	cameraFrame.GetUpVector(temp);
+	Vector3D cameraUp((GLfloat)temp[0], (GLfloat)temp[1], (GLfloat)temp[2]);
+
+	cameraFrame.GetForwardVector(temp);
+	Vector3D cameraDir((GLfloat)temp[0], (GLfloat)temp[1], (GLfloat)temp[2]);
+	cameraDir.normalize();
+
+	GLfloat vLength = (GLfloat)tanf((float)fovy / 2.0f) * nearPlane;
+	GLfloat hLength = vLength * (aspect);
+
+	Vector3D h = cameraDir.crossProduct(cameraUp).normalize().scalarMul(hLength); 
+	Vector3D v = cameraUp.normalize().scalarMul(vLength);
+
+	//update mPoint and mDirection
+	mPoint = cameraPos.add(cameraDir.scalarMul(nearPlane).add(h.scalarMul(mouseX).add(v.scalarMul(mouseY))));
+	mDirection = mPoint.sub(cameraPos);
+}
 
 ProjectionRay::~ProjectionRay()
 {
